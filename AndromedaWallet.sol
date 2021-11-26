2f699b59f4faf6ca947b9d2ffa42a1951c4b057c
ANDREA BARRA <andrea.barra001@studenti.uniparthenope.it>
2021-11-26 22:51:29 +0100

    AndromedaWallet solidity smart contract first commit

---
+++ AndromedaWallet.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+// This is an exercise for personal usage. I am trying to build my first flutter wallet for iOS and Android
+pragma solidity 0.8.7;
+
+contract Andromeda {
+    int balance;
+
+    constructor()  {
+        balance = 0;
+    }
+
+    function getBalance() view public returns(int){
+        return balance;
+    }
+
+    function depositToBalance(int addedAmount) public{
+        balance = balance + addedAmount;
+    }
+
+    function withdrawFromBalance(int withdrawnAmount) public{
+        balance = balance - withdrawnAmount;
+    }
+}
