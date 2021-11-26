8cd42c106a56258660c22fad7c419299b707e93d
ANDREA BARRA <andrea.barra001@studenti.uniparthenope.it>
2021-11-26 23:15:30 +0100

    Added flutter code

---
+++ Andromeda/lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Andromeda Wallet'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Client httpClient;
+  late Web3Client ethClient;
+  final myAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center());
+  }
+}
