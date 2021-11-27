6caa5eb8322edfc8e915926621750ad2aeabfe6b
ANDREA BARRA <andrea.barra001@studenti.uniparthenope.it>
2021-11-27 01:07:39 +0100

    Update source code with basic UI. Implemented bottom navigation bar and background color.

--- Andromeda/lib/main.dart
+++ Andromeda/lib/main.dart
@@ -1,54 +1,46 @@
-import 'dart:html';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:web3dart/web3dart.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(icon: new Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.account_balance_wallet), label: "Wallet"),
+          BottomNavigationBarItem(icon: new Icon(Icons.home), label: "Profile"),
+        ],
+        backgroundColor: Colors.white.withOpacity(0.9),
       ),
-      home: const MyHomePage(title: 'Andromeda Wallet'),
+      body: content(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late Client httpClient;
-  late Web3Client ethClient;
-  final myAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center());
-  }
+Widget content() {
+  return Container(
+    child: Column(
+      children: [
+        Container(
+            height: 230,
+            color: Colors.green,
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Balance",
+                style:
+                    GoogleFonts.varelaRound(fontSize: 60, color: Colors.white),
+              ),
+            ))
+      ],
+    ),
+  );
 }
